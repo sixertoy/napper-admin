7b8566318345ec1948f45c7e0315eed88edeadb6
sixertoy <sixertoy.github@gmail.com>
2019-07-05 06:15:08 +0200

    ⚙️  update packages with react resolution

--- .docz/app/imports.js
+++
@@ -1,6 +0,0 @@
-export const imports = {
-  'components/smart-layout/SmartLayout.mdx': () =>
-    import(
-      /* webpackPrefetch: true, webpackChunkName: "components-smart-layout-smart-layout" */ 'components/smart-layout/SmartLayout.mdx'
-    ),
-}
